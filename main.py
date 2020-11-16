397dd74cdf6ba394518b1c3314bffb3ca577fdcf
AlexeyKonoshenkov <73119298+AlexeyKonoshenkov@users.noreply.github.com>
2020-11-16 12:07:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import numpy as np
 
 def f1(x):
-    return (np.cos(x) + 0.05 * x**3 + np.log(2 * x**2))
+    return (np.cos(x) + 0.05 * x**3 + np.log2(x**2))
   
 def derivation(x, f):
-    eps = 0.0000001
-    return(round((f(x+eps) - f(x))/eps), 2)
+    eps = 0.00000001
+    return round(((f(x+eps)- f(x))/eps), 2)
   
 def f2(xar):
-    return (xar[0]**2/np.cos(xar[1]) + 0.05 * xar[1]**3 + 3 * xar[0]**3 / np.log(2 * xar[1]**2))
+    return ((xar[0]**2)*np.cos(xar[1]) + 0.05 * xar[1]**3 + 3 * xar[0]**3 * np.log2(xar[1]**2))
   
-def grad_func_2(x1, x2):
-    dx1 = 2 * x1/np.cos(x2) + 9 * x1**2/np.log(2 * x2**2)
-    dx2 = np.sin(x2) * x1**2 /((np.cos(x2))**2) + 0.15 * x2**2 - 6 * x1**3/x2 * (np.log(2 * x2**2))**2
+def grad_func_2(xar, f):
+    eps = 0.00000001
+    dx1 = (f([xar[0] + eps, xar[1]]) - f(xar))/eps
+    dx2 = (f([xar[0], xar[1] + eps]) - f(xar))/eps
     return [dx1, dx2]
   
 def gradient(xar, f):
@@ -33,15 +34,15 @@ def gradient_optimization_one_dim(f):
     print('Точка минимума х = ', x, ' Значение функции = ', f1(x), ' Количество шагов = ', step)
     return round(f1(x), 2)
 
-def gradient_optimization_two_dim(f):
+def gradient_optimization_multi_dim(f):
     eps = 0.01 
     xar = [4 , 10]
-    derar = grad_func_2(xar[0], xar[1])
+    derar = grad_func_2(xar, f2)
     step = 0
     while((derar[0] >= eps or derar[1] >= eps) and step < 50):
         for i in range(len(xar)):
             xar[i] = xar[i] - eps * derar[i]
-        derar = grad_func_2(xar[0], xar[1])
+        derar = grad_func_2(xar, f2)
         step += 1
     print('Точка минимума х = ', xar, ' Значение функции = ', f2(xar), ' Количество шагов = ', step)
     return round(f2(xar), 2)
