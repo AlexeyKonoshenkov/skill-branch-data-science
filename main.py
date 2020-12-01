f600f1fde9907ec62a5fc7142aa95c29b30e1297
AlexeyKonoshenkov <73119298+AlexeyKonoshenkov@users.noreply.github.com>
2020-12-01 15:03:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ def grad_func_2(xar, f):
   
 def gradient(xar, f):
     eps = 0.00000001
-    dx1 = round((f([xar[0] + eps, xar[1]]) - f(xar))/eps, 2)
-    dx2 = round((f([xar[0], xar[1] + eps]) - f(xar))/eps, 2)
+    dx1 = (f([xar[0] + eps, xar[1]]) - f(xar))/eps
+    dx2 = (f([xar[0], xar[1] + eps]) - f(xar))/eps
     #return round(np.sqrt(dx1**2 + dx2**2), 2)
     return [round(dx1, 2), round(dx2, 2)]
   
