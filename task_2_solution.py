d8f01cd9e8193f6c14a4c6017e07ee36f24992e6
AlexeyKonoshenkov <73119298+AlexeyKonoshenkov@users.noreply.github.com>
2021-01-22 06:32:22 +0000

    Create task_2_solution.py

---
+++ task_2_solution.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+def calculate_data_shape(x):
+    return x.shape
+    
+def take_columns(x):
+    return x.columns
+ 
+def calculate_target_ratio(x, target_name):
+    return round(x[target_name].mean(), 2)
+
+def calculate_data_dtypes(x):
+    return x.dtypes.value_counts()
+ 
+def calculate_cheap_apartment(x):
+    return len(x[x['price_doc'] <= 1000000])
+   
+def calculate_squad_in_cheap_apartment(x):
+    return round(x[x['price_doc'] <= 1000000]['full_sq'].mean(),0)
+    
+def calculate_mean_price_in_new_housing(x):
+    return round(x.loc[(x['build_year'] >= 2010) & (x['num_room'] == 3)]['price_doc'].mean(),0)
+    
+def calculate_mean_squared_by_num_rooms(x):
+    return np.around(x.groupby(['num_room'])['full_sq'].mean(), decimals = 2)
+   
+def calculate_squared_stats_by_materials(x):
+    data1 = np.around(x.groupby(['material'])['full_sq'].max(), decimals = 2)
+    data2 = np.around(x.groupby(['material'])['full_sq'].min(), decimals = 2)
+    return pd.concat([data1, data2], axis = 1)
+    
+def calculate_crosstab(x):
+    return np.around(x.pivot_table(values = ['price_doc'],  index = 'sub_area', columns = 'product_type', fill_value = 0, aggfunc = np.mean), 2)
